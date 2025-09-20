79b1bd48f6467e91e8c765f70c964803a71e3940
littlepe <565903071@qq.com>
2025-09-20 20:43:42 +0800

    Upload Files

---
+++ anykernel.sh
@@ -0,0 +1,68 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers & GitHub @ Xiaomichael
+
+### AnyKernel setup
+# global properties
+properties() { '
+kernel.string=KernelSU by KernelSU Developers | Build by HanShui_Official
+do.devicecheck=0
+do.modules=0
+do.systemless=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
+'; } # end properties
+
+### AnyKernel install
+## boot shell variables
+block=boot
+is_slot_device=auto
+ramdisk_compression=auto
+patch_vbmeta_flag=auto
+no_magisk_check=1
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh
+
+kernel_version=$(cat /proc/version | awk -F '-' '{print $1}' | awk '{print $3}')
+case $kernel_version in
+    5.1*) ksu_supported=true ;;
+    6.1*) ksu_supported=true ;;
+    6.6*) ksu_supported=true ;;
+    *) ksu_supported=false ;;
+esac
+
+ui_print "内核构建者: Coolapk@寒水Official"
+ui_print " " "  -> ksu_supported: $ksu_supported"
+$ksu_supported || abort "  -> Non-GKI device, abort."
+
+# boot install
+if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
+    split_boot # for devices with init_boot ramdisk
+    flash_boot # for devices with init_boot ramdisk
+else
+    dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+    write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+fi
+## end boot install
+# 优先选择模块路径
+if [ -f "$AKHOME/zram.zip" ]; then
+    MODULE_PATH="$AKHOME/zram.zip"
+    KSUD_PATH="/data/adb/ksud"
+    if [ -f "$KSUD_PATH" ]; then
+        ui_print "Installing zram Module..."
+        /data/adb/ksud module install "$MODULE_PATH"
+        ui_print "Installation Complete!"
+    else
+        ui_print "KSUD Not Found, skipping installation..."
+    fi
+else
+    ui_print "ZRAM module Not Found, skipping ZRAM module installation"
+fi
